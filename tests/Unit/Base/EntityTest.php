6cac7907ed668f8a9cd4bd4cde43c00ca0097711
Eric G. Huang <eric.g.yuan@gmail.com>
2019-09-09 23:23:46 +0800

    Add entity and value object base class

---
+++ tests/Unit/Base/EntityTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Unit\Base;
+
+use App\Base\Entity;
+use Tests\TestCase;
+
+class EntityTest extends TestCase
+{
+    public function testEquals_TwoIdentitiesAreTheSame_ReturnTrue(): void
+    {
+        $entityA = new MyEntity('123');
+        $entityB = new MyEntity('123');
+
+        $this->assertTrue($entityA->equals($entityB));
+    }
+
+    public function testEquals_TwoIdentitiesAreDifferent_ReturnFalse(): void
+    {
+        $entityA = new MyEntity('123');
+        $entityB = new MyEntity('124');
+
+        $this->assertFalse($entityA->equals($entityB));
+    }
+}
+
+class MyEntity extends Entity
+{
+    private $myId;
+
+    public function __construct(string $myId)
+    {
+        $this->myId = $myId;
+    }
+
+    public function getIdentity(): string
+    {
+        return $this->myId;
+    }
+}
