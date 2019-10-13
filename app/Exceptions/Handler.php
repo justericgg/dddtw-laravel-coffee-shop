d3f2f790bad33f9ea678ca864f7b494c3ca53847
Eric G. Huang <eric.g.yuan@gmail.com>
2019-10-13 10:25:51 +0800

    Add domain error handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,11 @@
 
 namespace App\Exceptions;
 
+use Common\DomainException;
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class Handler extends ExceptionHandler
 {
@@ -29,7 +32,7 @@ class Handler extends ExceptionHandler
     /**
      * Report or log an exception.
      *
-     * @param  \Exception  $exception
+     * @param Exception $exception
      * @return void
      */
     public function report(Exception $exception)
@@ -40,12 +43,22 @@ class Handler extends ExceptionHandler
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
+     * @param  Request  $request
+     * @param Exception $exception
+     * @return Response
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof DomainException) {
+            $error = [
+                'error' => [
+                    'code' => $exception->getCode(),
+                    'msg' => $exception->getMessage(),
+                ],
+            ];
+            return response(json_encode($error), 400);
+        }
+
         return parent::render($request, $exception);
     }
 }
